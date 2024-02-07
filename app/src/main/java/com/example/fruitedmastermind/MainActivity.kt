d89f1631aae247efa7d85708f4cd34c662166117
Killian <killian.carvalho@protonmail.com>
2024-02-07 11:59:55 +0100

    Feat: add the check_combination function

--- app/src/main/java/com/example/fruitedmastermind/MainActivity.kt
+++ app/src/main/java/com/example/fruitedmastermind/MainActivity.kt
@@ -3,20 +3,32 @@ package com.example.fruitedmastermind
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,7 +52,6 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     MasterMind()
-
                 }
             }
         }
@@ -52,6 +63,32 @@ fun set_combination(): List<String> {
     val combination = fruits.shuffled().take(4)
 
     return combination
+}
+
+fun check_combination(guess_combination:List<String>, user_combination: List<String>): MutableList<Char>
+{
+    var nbr_good_position = 0
+    var nbr_bad_place = 0
+    var result_proposition: MutableList<Char> = mutableListOf()
+    var cnt:Int = 0
+
+    for (fruit in user_combination)
+    {
+        val check_value = guess_combination.find { it == fruit }
+        if (check_value == null) {
+            result_proposition.add('X')
+            continue
+        }
+        else {
+            if (guess_combination[cnt] == user_combination[cnt])
+            {
+                result_proposition.add('1')
+            }
+            else
+                result_proposition.add('0')
+        }
+    }
+    return result_proposition
 }
 
 @Composable
@@ -130,20 +167,6 @@ fun GameScreen() {
                 modifier = Modifier.size(70.dp)
             )
         }
-    }
-}
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FruitedMastermindTheme {
-        Greeting("Android")
+        var combination = listOf<String>("Banana", "Raspberry", "Orange", "Plum")
     }
 }
