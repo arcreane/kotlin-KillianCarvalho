15b03f84d868dd5c07a346cbf0cdae179f4999ea
Killian <killian.carvalho@protonmail.com>
2024-02-08 14:47:47 +0100

    Fix: remove the unused package

--- app/src/main/java/com/example/fruitedmastermind/HomeScreen.kt
+++ app/src/main/java/com/example/fruitedmastermind/HomeScreen.kt
@@ -1,32 +1,23 @@
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun Home_screen(nav_controller: NavController) {
     Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Bienvenue dans le jeu Mastermind !", style = MaterialTheme.typography.h4)
+        Text(text = "Bienvenue dans le jeu Mastermind !")
         Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = { nav_controller.navigate("game") }) {
             Text(text = "Commencer le jeu")
         }
     }
-}
-
-@Preview
-@Composable
-fun Home_screen_preview() {
-    Home_screen()
 }
